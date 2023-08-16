8a036ca9c29ee7b26888ba12b9781b9018cce31b
Sreekar617 <hindi6476@gmail.com>
2023-08-16 17:26:14 -0500

    do i need a webserver?

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-console.log(ctx);
-const canvas_width = canvas.width = 600;
-const canvas_height = canvas.height = 600;
+const SPRITE_WIDTH = 13;
+const SPRITE_HEIGHT = 14;
+const BORDER_WIDTH = 1;
+const SPACING_WIDTH = 1;
 
-const playerSprites = new Image();
-playerSprites.src = 'mario.png';
+function spritePositionToImagePosition(row, col) {
+    return {
+        x: (
+            BORDER_WIDTH +
+            col * (SPACING_WIDTH + SPRITE_WIDTH)
+        ),
+        y: (
+            BORDER_WIDTH +
+            row * (SPACING_WIDTH + SPRITE_HEIGHT)
+        )
+    }
+}
 
-function startAnimation() {
-    ctx.clearRect(0, 0, canvas_width, canvas_height)
-    ctx.fillRect(50,50,100,100)
-    requestAnimationFrame(startAnimation);
+var canvas = document
+            .querySelector('canvas');
+var context = canvas
+              .getContext('2d');
+
+var spriteSheetURL = 'https://codehs.com/uploads/e4cfb06e001bd92cf41139928e88819a';
+var image = new Image();
+image.src = spriteSheetURL;
+image.crossOrigin = true;
+
+// extract all of our frames
+var karelright0 = spritePositionToImagePosition(0, 0);
+var karelright1 = spritePositionToImagePosition(0, 1);
+var karelright2 = spritePositionToImagePosition(0, 2);
+var karelleftt0 = spritePositionToImagePosition(1, 0);
+var karelleft1 = spritePositionToImagePosition(1, 1);
+var karelleft2 = spritePositionToImagePosition(1, 2);
+
+var walkCycle = [
+    karelright0,
+    karelright1,
+    karelright2,
+    karelright1
+];
+
+var frameIndex = 0;
+var frame;
+function animate() {
+    // once we hit the end of the cycle,
+    // start again
+    if (frameIndex === walkCycle.length) {
+        frameIndex = 0;
+    }
+    frame = walkCycle[frameIndex];
+    context.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+    context.drawImage(
+        image,
+        frame.x,
+        frame.y,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT,
+        0,
+        0,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT
+    );
+    frameIndex += 1;
+}
+
+image.onload = function() {
+    setInterval(animate, 250);
 };
-startAnimation();
